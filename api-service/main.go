689120c6e37c9879a6f22f5f57de632b7a969d5e
Abel de la Paz <abel45991690@gmail.com>
2022-11-21 21:43:40 +0100

    chore: api-service and db connection

--- api-service/main.go
+++ api-service/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type SateliteInfo struct {
+	gorm.Model
+	Text string `json:"text"`
+}
+
 func main() {
+	env := os.Getenv("PROD_ENV")
+
+	if env == "" {
+		godotenv.Load(".env")
+	}
+
 	r := gin.Default()
+
+	dbURL := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	db.AutoMigrate(&SateliteInfo{})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
