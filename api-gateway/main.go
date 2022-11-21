3b48c7cbc370d9231cfbf706851985dda042d530
Abel de la Paz <abel45991690@gmail.com>
2022-11-21 20:49:31 +0100

    build: add Dockerfiles

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Abeldlp/fullinfo/api-gateway/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	env := os.Getenv("PROD_ENV")
+
+	if env == "" {
+		godotenv.Load(".env")
+	}
+
 	r := gin.Default()
 
-	r.Any("/satelite/*proxyPath", handlers.ProxyTo("http://localhost:8000"))
+	r.Any("/satelite/*proxyPath", handlers.ProxyTo(os.Getenv("API_SERVICE_URL")))
 
 	r.Run(":8080")
 }
